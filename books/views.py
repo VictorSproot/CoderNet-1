b8600ba0a2852be3c5e087ad92bf9c72e65b63cd
neverdieone <altiore@mail.ru>
2018-12-03 12:05:58 +0300

    Fix RSS.

--- books/views.py
+++ books/views.py
@@ -64,7 +64,8 @@ class Rss(Feed):
     link = '/'
 
     def items(self):
-        qs = list(Book.objects.all()) + list(Course.objects.all()) + list(Articles.objects.all())
+        # qs = list(Book.objects.all()) + list(Course.objects.all()) + list(Articles.objects.all())
+        qs = Book.objects.all()
         return qs
 
     def item_title(self, item):
