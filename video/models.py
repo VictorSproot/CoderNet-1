cb6d8773978d31a514ab0aa0f4de42b968dcfd90
VictorSproot <89292844844@mail.ru>
2019-01-03 01:34:01 +0300

    Update models.py

--- video/models.py
+++ video/models.py
@@ -74,7 +74,7 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-
+        ordering = ['title']
 
 class Comments(models.Model):
     class Meta:
